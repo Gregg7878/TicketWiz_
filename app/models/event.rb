28359a89431fd5b1d8084c91596a322638b3ad59
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-10-12 09:14:30 +0300

    Added Validations to events.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :organiser
   belongs_to :customer
+
+  # Validations
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_date_in_future
+
 end
